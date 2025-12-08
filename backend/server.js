d5a5d4d61d2eaf907d162648a56d37463dac339b
Project_AnonyMate <officialshobhitraj@gmail.com>
2025-12-08 23:18:47 +0530

    First version v1

---
+++ backend/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+let waitingQueue = []; 
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on("join_queue", () => {
+        if (waitingQueue.length > 0) {
+            const partnerSocket = waitingQueue.pop(); 
+            const roomId = `${socket.id}#${partnerSocket.id}`;
+
+            socket.join(roomId);
+            partnerSocket.join(roomId);
+
+            io.to(socket.id).emit("match_found", { roomId, initiator: true });
+            io.to(partnerSocket.id).emit("match_found", { roomId, initiator: false });
+
+            console.log(`Matched ${socket.id} with ${partnerSocket.id}`);
+        } else {
+            waitingQueue.push(socket);
+            console.log(`User ${socket.id} added to queue.`);
+        }
+    });
+
+    socket.on("signal", (data) => {
+        socket.to(data.roomId).emit("signal", data.signalData);
+    });
+
+    socket.on("disconnect", () => {
+        waitingQueue = waitingQueue.filter(s => s.id !== socket.id);
+    });
+});
+
+server.listen(3001, () => console.log(`AnonyMate Server running on port 3001`));
