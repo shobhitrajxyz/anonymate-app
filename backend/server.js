ecf5464c218ef72d45509e96edac601670e61783
Project_AnonyMate <officialshobhitraj@gmail.com>
2025-12-08 23:29:35 +0530

    Ready for cloud

--- backend/server.js
+++ backend/server.js
@@ -47,4 +47,5 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3001, () => console.log(`AnonyMate Server running on port 3001`));
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
